fb22f691ae438ec39af953f49d882196341f95ff
Ronald Theodoro <ronaldtheodoro@gmail.com>
2021-02-18 20:19:33 -0300

    Add StackSymbol

---
+++ algorithms/stacks/stack-symbol.js
@@ -0,0 +1,32 @@
+const _items = Symbol('stackItems')
+
+class StackSymbol {
+
+    constructor() {
+        this[_items] = []
+    }
+
+    push(element) {
+        this._items.push(element)
+    }
+
+    pop() {
+        return this._items.pop()
+    }
+
+    peek() {
+        return this._items[this._items.length - 1]
+    }
+
+    isEmpty() {
+        return this._items.length === 0
+    }
+
+    clear() {
+        this._items = []
+    }
+
+    size() {
+        return this._items.length
+    }
+}
