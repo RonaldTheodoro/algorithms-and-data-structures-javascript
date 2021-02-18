439254a4c3d24e8e85d8865f7960f2b11653834e
Ronald Theodoro <ronaldtheodoro@gmail.com>
2021-02-18 20:55:36 -0300

    Add baseConverter with tests

---
+++ src/algorithms/stacks/stack-base-converter.js
@@ -0,0 +1,25 @@
+const Stack = require('../../../algorithms/stacks/stack')
+
+function baseConverter(decimalNumber, base) {
+    const stack = new Stack()
+    const digits = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    let number = decimalNumber
+    let rem
+    let baseString = ''
+    if(!(base >= 2 && base <= 36)) {
+        return ''
+    }
+    while (number > 0) {
+        rem = Math.floor(number % base)
+        stack.push(rem)
+        number = Math.floor(number / base)
+    }
+
+    while (!stack.isEmpty()) {
+        baseString += digits[stack.pop()]
+    }
+
+    return baseString
+}
+
+module.exports = baseConverter
---
+++ tests/src/algorithms/stacks/stack-base-converter.test.js
@@ -0,0 +1,15 @@
+const baseConverter = require(
+    '../../../../src/algorithms/stacks/stack-base-converter'
+)
+
+describe('Base converter tests', () => {
+
+    test('Should convert decimal base', () => {
+        expect(baseConverter(100345, 2)).toEqual('11000011111111001')
+        expect(baseConverter(100345, 8)).toEqual('303771')
+        expect(baseConverter(100345, 16)).toEqual('187F9')
+        expect(baseConverter(100345, 35)).toEqual('2BW0')
+        expect(baseConverter(100345, 40)).toEqual('')
+    })
+
+})
