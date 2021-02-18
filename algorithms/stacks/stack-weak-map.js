cf00b9cc14c23e44cedd531f3c00461f9267831f
Ronald Theodoro <ronaldtheodoro@gmail.com>
2021-02-18 20:23:20 -0300

    Add stack weak map

---
+++ algorithms/stacks/stack-weak-map.js
@@ -0,0 +1,34 @@
+const items = new WeakMap()
+
+class StackWeakMap {
+
+    constructor() {
+        items.set(this, [])
+    }
+
+    push(element) {
+        const s = items.get(this)
+        s.push(element)
+    }
+
+    pop() {
+        const s = items.get(this)
+        return s.pop()
+    }
+
+    peek() {
+        return this._items[this._items.length - 1]
+    }
+
+    isEmpty() {
+        return this._items.length === 0
+    }
+
+    clear() {
+        this._items = []
+    }
+
+    size() {
+        return this._items.length
+    }
+}
